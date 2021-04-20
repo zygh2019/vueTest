0ed1c1f45bc19cfe1644646866c7ef9014423369
shengbinliu <203003747@qq.com>
2021-04-20 20:31:13 +0800

    提交没上传的文件

---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="hello">
+		<p>我是vip</p>
+		<h1>{{ msg }}</h1>
+		<h2>{{yes}}</h2>
+		<bo
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HelloWorld',
+		props: {
+			msg: String,
+			yes:String
+			
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	p{
+		font-size: 0.625rem;
+		background-color: firebrick;
+	}
+	h1{
+		background-color: yellowgreen;
+	}
+</style>
